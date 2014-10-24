d847260918e8e2513c0f83730edfe032b8f6d79f
Andrew de Andrade <aandrade@uber.com>
2014-10-24 00:57:42 +0000

    Cleaning everything

--- test/error-to-file-stream.tape.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var test = require('tape');
-var errorsToFilesTransformStream = require('../lib/error-to-file-stream');
-
-test('Error Messages to File Messages Transform Stream', function runTests(t) {
-    t.plan(1);
-    t.ok(errorsToFilesTransformStream, 'Transform Function exists');
-});
