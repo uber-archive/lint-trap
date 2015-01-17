eca3fcd676fd6499ba83b554cd6c40c09a6afc10
Andrew de Andrade <aandrade@uber.com>
2015-01-16 16:33:54 -0800

    Disable no-catch-shadow and W002

--- reporters/jshint.js
+++ reporters/jshint.js
@@ -4,17 +4,21 @@ var process = require('process');
 function writeError(result) {
     var error = result.error;
 
-    process.stdout.write(JSON.stringify({
-        type: 'error',
-        linter: 'jshint',
-        file: result.file,
-        error: error,
-        line: error.line,
-        column: error.character,
-        rule: error.code,
-        message: error.reason
-    }));
-    process.stdout.write(',');
+    // JSHint gives no option to disable rule W002. Just skip it here.
+    // Hack. sad. :(
+    if (error.code !== 'W002') {
+        process.stdout.write(JSON.stringify({
+            type: 'error',
+            linter: 'jshint',
+            file: result.file,
+            error: error,
+            line: error.line,
+            column: error.character,
+            rule: error.code,
+            message: error.reason
+        }));
+        process.stdout.write(',');
+    }
 }
 
 function reporter(results, data, opts) {
